2d07eaf36a44901cf8d83a48cc7322de25958527
pabloconejos <PABLO.CONEJOS.CHIRIVELLA@CIEEP.COM>
2022-12-01 13:28:46 +0100

    Initial Commit

---
+++ 02ejerCrearExamen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  02ejerCrearExamen
+//
+//  Created by Pablo Conejos Chirivella on 01/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
